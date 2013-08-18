58e0a317b143f036b8db8b4de93b3e97aa62b933
Sasi kumar <sasikt@thoughtworks.com>
2013-08-18 13:57:36 +0900

    Added code to open camera from app

--- src/com/abc/MyActivity.java
+++ src/com/abc/MyActivity.java
@@ -1,15 +1,35 @@
 package com.abc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    private Button CameraCapture;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        CameraCapture = (Button)findViewById(R.id.camerabutton) ;
+        CameraCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, 0);
+
+            }
+
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)    {
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            String result = data.toURI();
+            System.out.print(result);
+
+        }
     }
 }
